d645091dce88954b6e9e5b9b4e2d4a1e70d640cf
Scott Walter <scott.walter@nice.com>
2023-05-05 08:15:41 -0500

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { of } from 'rxjs';
+import { catchError, ignoreElements } from 'rxjs/operators';
+import { UserService } from '../shared/data-access/user/user.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  // user$ = this.userService.getUser();
+  user$ = this.userService.getUserWithError();
+  userError$ = this.user$.pipe(
+    ignoreElements(),
+    catchError((err) => of(err))
+  );
+
+  constructor(public userService: UserService) {}
+}
